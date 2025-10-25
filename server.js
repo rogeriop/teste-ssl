77c7fff95b6fbf89df065af9e6ce47815558227f
rogeriop <pinheiroxrogerio@gmail>
2025-10-25 15:53:24 -0300

    Correção maleabilidade publicação em diferentes ambientes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import 'dotenv/config';
 import app from "./src/app.js";
 import http from 'http';
 
-const PORT = process.env.PORT || 3030;
+const AMBIENTE = process.env.AMBIENTE || 'DESEN';
+
+const PORT = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
+  console.log(`Ambiente: ${AMBIENTE}`);
   console.log(`Servidor escutando na porta ${PORT}!`);
 });
 
